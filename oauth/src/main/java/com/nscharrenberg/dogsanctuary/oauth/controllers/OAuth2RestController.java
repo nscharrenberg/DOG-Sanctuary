c4e0001f90a4ea0b93325b0d4779547c5987b430
Noah Scharrenberg <nscharrenberg@hotmail.com>
2019-06-03 12:52:54 +0200

    outh configuration

---
+++ oauth/src/main/java/com/nscharrenberg/dogsanctuary/oauth/controllers/OAuth2RestController.java
@@ -0,0 +1,9 @@
+package com.nscharrenberg.dogsanctuary.oauth.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import com.security.oauth.service.UserService;
+
+@RestController
+public class OAuth2RestController {
+
+}
